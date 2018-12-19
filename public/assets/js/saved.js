2ed83a46773ff2ddfdaecd02c15375547c88f312
aOpenbrier <aopenbrier@gmail.com>
2018-12-19 01:37:38 -0800

    saved card styling

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -5,7 +5,7 @@ fetch('/savedarticles')
         document.getElementById('savednews').innerHTML = ''
         r.forEach((article, index) => {
             const articleCard = document.createElement('div')
-            articleCard.className = 'row mb-2'
+            articleCard.className = 'row mb-3'
             articleCard.id = `card${index}`
             articleCard.innerHTML = `<div class='card'><div class="card-body">
                 <a class="card-link float-right btn btn-danger" data-object='${JSON.stringify(article)}' onclick='deleteArticle(this)'>Delete</a>
@@ -13,34 +13,54 @@ fetch('/savedarticles')
                 <p class="card-text">${article.summary}</p>
                 <p class='cart-text text-muted small'>${article.date}</p>
             </div>
-            <div class="card-footer text-muted">
+            <div class="card-footer py-1">
                 <form>
-                    <label class='form-control' for='notesinput${index}'><h5>Notes:</h5></label
-                    <a class="card-link btn float-right" data-object='${JSON.stringify(article)}' onclick='editNotes(this)'>Edit</a>
-                    <input class='form-control' id='notesinput${index}' type='text' disabled>example</input>
-                    <button type="submit" class="btn btn-primary" onlick='saveNotes(this)'>Save</button>
+                        <div class='d-flex'><label for='notesinput${index}' class='mr-auto mb-0 align-self-center'><h6 class='mb-0'>Notes:</h6></label>
+                        <button id='editBtn${index}' class="btn btn-primary mx-2" data-cardIndex='${index}' onclick='editNotes(this)'>Edit</button>
+                        <button id='saveBtn${index}' class="btn btn-primary" data-cardIndex='${index}' onclick='saveNotes(this)' disabled>Save</button>
+                        </div>
+                        <input class='form-control' id='notesinput${index}' type='text' value='example' disabled>
                 </form>
             </div>
             </div>
             `
-            document.getElementById('scrapednews').appendChild(articleCard)
+            document.getElementById('savednews').appendChild(articleCard)
         })
     })
     .catch(e => console.log(e))
 
 function deleteArticle(elem){
+
     // Send delete request to server
     // TODO
 
     // Remove element from DOM
     // TODO
+
     }
 
 function editNotes(elem){
-    // enable form editing
+    event.preventDefault()
+    // enable input
+    const cardIndex = elem.getAttribute('data-cardIndex')
+    document.getElementById(`notesinput${cardIndex}`).removeAttribute('disabled')
+    // enable save button
+    document.getElementById(`saveBtn${cardIndex}`).removeAttribute('disabled')
+    // disable edit button
+    document.getElementById(`editBtn${cardIndex}`).setAttribute('disabled', true)
  }
 
 function saveNotes(elem){
-    //  send update request to server 
+    event.preventDefault()
+    //  send update request to server
+
+    // disable save button
+    const cardIndex = elem.getAttribute('data-cardIndex')
+    document.getElementById(`saveBtn${cardIndex}`).setAttribute('disabled', true)
+    
+    // disable form
+    document.getElementById(`notesinput${cardIndex}`).setAttribute('disabled', true)
+    // renable edit button
+    document.getElementById(`editBtn${cardIndex}`).removeAttribute('disabled')
  }
 
