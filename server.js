653eea5775f2eb21b4947d356fdfb4a171854865
aOpenbrier <aopenbrier@gmail.com>
2018-12-20 11:50:34 -0800

    mongo connection

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const mongodbURI = process.env.MONGODB_URI || 'mongodb://localhost/mongoscraper'
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+mongoose.connect(mongodbURI, { useNewUrlParser: true })
 
 require('./routes')(app)
 
