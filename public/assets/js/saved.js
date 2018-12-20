d5e2923ed3156bd552a6affe252975ebdcebfb26
aOpenbrier <aopenbrier@gmail.com>
2018-12-20 11:55:09 -0800

    requests for delete and update notes

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -1,14 +1,13 @@
 fetch('/savedarticles')
     .then(r => r.json())
     .then(r => {
-        console.log(r)
         document.getElementById('savednews').innerHTML = ''
         r.forEach((article, index) => {
             const articleCard = document.createElement('div')
             articleCard.className = 'row mb-3'
             articleCard.id = `card${index}`
             articleCard.innerHTML = `<div class='card'><div class="card-body">
-                <a class="card-link float-right btn btn-danger" data-object='${JSON.stringify(article)}' onclick='deleteArticle(this)'>Delete</a>
+                <button class="float-right btn btn-danger" data-id='${article._id}' onclick='deleteArticle(this)'>Delete</button>
                 <h5 class="card-title"><a href='${article.url}'>${article.headline}</a></h5>
                 <p class="card-text">${article.summary}</p>
                 <p class='cart-text text-muted small'>${article.date}</p>
@@ -17,9 +16,9 @@ fetch('/savedarticles')
                 <form>
                         <div class='d-flex'><label for='notesinput${index}' class='mr-auto mb-0 align-self-center'><h6 class='mb-0'>Notes:</h6></label>
                         <button id='editBtn${index}' class="btn btn-primary mx-2" data-cardIndex='${index}' onclick='editNotes(this)'>Edit</button>
-                        <button id='saveBtn${index}' class="btn btn-primary" data-cardIndex='${index}' onclick='saveNotes(this)' disabled>Save</button>
+                        <button id='saveBtn${index}' class="btn btn-primary" data-id='${article._id}' data-cardIndex='${index}' onclick='saveNotes(this)' disabled>Save</button>
                         </div>
-                        <input class='form-control' id='notesinput${index}' type='text' value='example' disabled>
+                        <input class='form-control' id='notesinput${index}' type='text' value='${article.notes ? article.notes : '' }' disabled>
                 </form>
             </div>
             </div>
@@ -32,7 +31,11 @@ fetch('/savedarticles')
 function deleteArticle(elem){
 
     // Send delete request to server
-    // TODO
+    fetch(`savedArticles/${elem.getAttribute('data-id')}`, {
+        method: 'DELETE'
+    })
+    .then(r => console.log(r))
+    .catch(e => console.error(e))
 
     // Remove element from DOM
     // TODO
@@ -53,11 +56,20 @@ function editNotes(elem){
 function saveNotes(elem){
     event.preventDefault()
     //  send update request to server
+    const cardIndex = elem.getAttribute('data-cardIndex')
+    const notes = document.getElementById(`notesinput${cardIndex}`).value
+    fetch(`/savedarticles/${elem.getAttribute('data-id')}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({"notes": notes})
+    })
+    .then(r => console.log(r))
+    .catch(e => console.log(e))
 
     // disable save button
-    const cardIndex = elem.getAttribute('data-cardIndex')
     document.getElementById(`saveBtn${cardIndex}`).setAttribute('disabled', true)
-    
     // disable form
     document.getElementById(`notesinput${cardIndex}`).setAttribute('disabled', true)
     // renable edit button
