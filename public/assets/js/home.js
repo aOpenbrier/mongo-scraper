bfbba85a26b3e9f78258e9011f3a265c0b8c92c6
aOpenbrier <aopenbrier@gmail.com>
2018-12-20 11:56:22 -0800

    prevent page reload

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -2,16 +2,15 @@
 fetch('/newarticles')
     .then(r => r.json())
     .then(r => {
-        console.log(r)
         document.getElementById('scrapednews').innerHTML = ''
         r.forEach((article, index) => {
             const articleCard = document.createElement('div')
             articleCard.className = 'row mb-2'
-            articleCard.innerHTML = `<div class='card'><div class="card-body">
+            articleCard.innerHTML = `<div class='card w-100'><div class="card-body">
+            <button class="btn btn-primary float-right" data-object='${JSON.stringify(article)}' onclick='saveArticle(this)'>Save</button>
             <h5 class="card-title"><a href='${article.url}'>${article.headline}</a></h5>
             <p class="card-text">${article.summary}</p>
             <p class='cart-text text-muted small'>${article.date}</p>
-            <a class="card-link btn float-right" data-object='${JSON.stringify(article)}' onclick='saveArticle(this)'>Save</a>
             </div></div>
             `
             document.getElementById('scrapednews').appendChild(articleCard)
@@ -20,16 +19,16 @@ fetch('/newarticles')
     .catch(e => console.log(e))
 
 function saveArticle(elem) {
+    event.preventDefault()
     // send article to server to save to db
     fetch('/savedarticles', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset=utf-8'
-        },            // 'Content-Type' key needs to be wrapped in quotes since it has a hyphen
+        },
         body: elem.getAttribute('data-object')
     })
         .then(r => { console.log(r) })
         .catch(e => { console.error(e) })
 
-console.log('this will save the article')
 }
