e5f2dad41ffa36056e18109ea089534e3b286317
aOpenbrier <aopenbrier@gmail.com>
2018-12-22 19:54:53 -0800

    Delete All button

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -7,7 +7,7 @@ fetch('/savedarticles')
             articleCard.className = 'row mb-3'
             articleCard.id = `card${index}`
             articleCard.innerHTML = `<div class='card w-100'><div class="card-body">
-                <button class="float-right btn btn-danger" data-id='${article._id}' data-cardIndex='${index}' onclick='deleteArticle(this)'>Delete</button>
+                <button class="float-right btn btn-danger" data-dbId='${article._id}' data-htmlId='${index}' onclick='deleteArticle(this)'>Delete</button>
                 <h5 class="card-title"><a href='${article.url}'>${article.headline}</a></h5>
                 <p class="card-text">${article.summary}</p>
                 <p class='cart-text text-muted small'>${article.date.split('T')[0]}</p>
@@ -15,8 +15,8 @@ fetch('/savedarticles')
             <div class="card-footer py-1">
                 <form>
                         <div class='d-flex'><label for='notesinput${index}' class='mr-auto mb-0 align-self-center'><h6 class='mb-0'>Notes:</h6></label>
-                        <button id='editBtn${index}' class="btn btn-primary mx-2" data-cardIndex='${index}' onclick='editNotes(this)'>Edit</button>
-                        <button id='saveBtn${index}' class="btn btn-primary" data-id='${article._id}' data-cardIndex='${index}' onclick='saveNotes(this)' disabled>Save</button>
+                        <button id='editBtn${index}' class="btn btn-primary mx-2" data-htmlId='${index}' onclick='editNotes(this)'>Edit</button>
+                        <button id='saveBtn${index}' class="btn btn-primary" data-dbId='${article._id}' data-htmlId='${index}' onclick='saveNotes(this)' disabled>Save</button>
                         </div>
                         <input class='form-control' id='notesinput${index}' type='text' value='${article.notes ? article.notes : '' }' disabled>
                 </form>
@@ -29,22 +29,21 @@ fetch('/savedarticles')
     .catch(e => console.log(e))
 
 function deleteArticle(elem){
-
     // Send delete request to server
-    fetch(`savedArticles/${elem.getAttribute('data-id')}`, {
+    fetch(`savedArticles/${elem.getAttribute('data-dbId')}`, {
         method: 'DELETE'
     })
     .then(r => {
         // Remove element from DOM
-        document.getElementById(`card${elem.getAttribute('data-cardIndex')}`).remove()
-        console.log(r)
+        document.getElementById(`card${elem.getAttribute('data-htmlId')}`).remove()
     })
     .catch(e => console.error(e))
+}
 
 function editNotes(elem){
     event.preventDefault()
     // enable input
-    const cardIndex = elem.getAttribute('data-cardIndex')
+    const cardIndex = elem.getAttribute('data-htmlId')
     document.getElementById(`notesinput${cardIndex}`).removeAttribute('disabled')
     // enable save button
     document.getElementById(`saveBtn${cardIndex}`).removeAttribute('disabled')
@@ -55,24 +54,34 @@ function editNotes(elem){
 function saveNotes(elem){
     event.preventDefault()
     //  send update request to server
-    const cardIndex = elem.getAttribute('data-cardIndex')
+    const cardIndex = elem.getAttribute('data-htmlId')
     const notes = document.getElementById(`notesinput${cardIndex}`).value
-    fetch(`/savedarticles/${elem.getAttribute('data-id')}`, {
+    fetch(`/savedarticles/${elem.getAttribute('data-dbId')}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json; charset=utf-8'
         },
         body: JSON.stringify({"notes": notes})
     })
-    .then(r => console.log(r))
-    .catch(e => console.log(e))
+    .then(r => console.log(r)).catch(e => console.log(e))
 
     // disable save button
     document.getElementById(`saveBtn${cardIndex}`).setAttribute('disabled', true)
-   
     // disable form
     document.getElementById(`notesinput${cardIndex}`).setAttribute('disabled', true)
     // renable edit button
     document.getElementById(`editBtn${cardIndex}`).removeAttribute('disabled')
+ }
+
+ function deleteAllSaved() {
+     // Send delete request to server
+     fetch(`savedArticles`, {
+         method: 'DELETE'
+     })
+    .then(r => {
+        console.log(r)
+        document.getElementById('savednews').innerHTML = ''
+    })
+        .catch(e => console.log(e))
  }
 
